ae28555054a147e3cf34a78a9c252db05cda9920
Rohitp100 <106956998+Rohitp100@users.noreply.github.com>
2024-02-26 16:12:26 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,22 +19,21 @@ const Projects = () => {
       You can use it as a customer or a seller. As a customer you would be able to customize the appearance of your products add them in your wishlist or cart. Seller would be able to put the products quantity and pricing. 
      </p>
      <p style={{fontSize: '36px', paddingBottom:'30px'}} className='text-rose-700'>
-      AI-POWERED 3D WEBSITE
+     <a href="https://github.com/Rohitp100/AI_POWERED_3D-WEBSITE" style={{textDecoration: 'none', color: 'inherit'}}>AI-POWERED 3D WEBSITE</a>
      </p>
      <p style={{fontSize: '24px',paddingBottom:'30px'}} className='text-rose-900'>
      You would be able to customize different sections of your PS5. You can change colour or apply skin of your choice using AI or the image files you already have. 
-     [React]
      </p>
 
      <p style={{fontSize: '36px', paddingBottom:'30px'}} className='text-rose-700'>
-       SUSHI WEBSITE
+     <a href="https://github.com/Rohitp100/SUSHI_WEBSITE" style={{textDecoration: 'none', color: 'inherit'}}>SUSHI WEBSITE</a>
      </p>
      <p style={{fontSize: '24px',paddingBottom:'30px'}} className='text-rose-900'>
        Its a fully responsive HTML & CSS website with animation.
      </p>
 
      <p style={{fontSize: '36px', paddingBottom:'30px'}} className='text-rose-700'>
-      BOOK STORE
+     <a href="https://github.com/Rohitp100/BOOK_STORE" style={{textDecoration: 'none', color: 'inherit'}}>BOOK STORE</a>
      </p>
      <p style={{fontSize: '24px',paddingBottom:'30px'}} className='text-rose-900'>
       Its a MERN stack website with CRUD and Router.
